f814b98d20f1ec1a8058dfa3b223f84e1d1a0ec9
Rajeet <rajeetdps@gmail.com>
2021-07-16 20:51:09 +0530

    Adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar'; 
 
 function App() {
   return (
     <div className="app">
       <Header/>
-
-     {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
+    <div className="app_body">
+      <Sidebar/>
+       {/* Feed */}
       {/* Widgets */}
+    </div>
+     
     </div>
   );
 }
