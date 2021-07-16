c6e8e08af4051d7a7c6dcf784358891e4765eab3
Rajeet <rajeetdps@gmail.com>
2021-07-16 20:05:55 +0530

    Styled Header right

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,10 @@ import SearchIcon from '@material-ui/icons/Search';
 const Header = () => {
     return (
         <div className="Header">
-            <h1>header</h1>
             <div className="header_left">
-                <img src="" alt="" />
+                <img 
+                src="https://image.flaticon.com/icons/png/512/174/174857.png" 
+                alt="" />
                 <div className="header_search">
                     <SearchIcon/>
                     <input type="text" />
