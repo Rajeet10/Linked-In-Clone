7989a71b5ece8b0d2eedc265bc86388d55989722
Rajeet <rajeetdps@gmail.com>
2021-07-17 20:11:36 +0530

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import Sidebar from './Sidebar'; 
+import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  // const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Header/>
-    <div className="app_body">
-      <Sidebar/>
-       <Feed/>
-      {/* Widgets */}
-    </div>
-     
+      <Header />
+      <Login/>
+      {/* {!user ? <Login/> : ( */}
+        {/* <div className="app_body"> */}
+          {/* <Sidebar /> */}
+          {/* <Feed /> */}
+          {/* Widgets */}
+        {/* </div> */}
+      {/* )} */}
     </div>
   );
 }
