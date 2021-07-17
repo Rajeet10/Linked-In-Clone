e9d7ee8e447e217d2dccf74485d06e20158777b6
Rajeet <rajeetdps@gmail.com>
2021-07-17 14:35:05 +0530

    Added Input icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
-import CreateIcon from '@material-ui/icons/Create'
+import CreateIcon from '@material-ui/icons/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventIcon from '@material-ui/icons/Event';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
 const Feed = () => {
     return (
@@ -9,9 +14,15 @@ const Feed = () => {
                 <div className="feed_input">
                     <CreateIcon/>
                     <form>
-                        <input type="text" />
+                        <input type="text" placeholder="Start a post"/>
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                    <InputOption Icon={EventIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
             
