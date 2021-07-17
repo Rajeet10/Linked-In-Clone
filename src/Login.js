39207d96ead0c42f0a8fd8cbb8231f20a0462bc9
Rajeet <rajeetdps@gmail.com>
2021-07-17 20:24:48 +0530

    Building login page

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './Login.css';
 
 const Login = () => {
+    const register=()=>{
+
+    };
     return (
         <div className="login">
-            <h1>Not</h1>
+           <img src="https://www.amocrm.com/static/images/pages/integrations/logo/linkedin.png"  alt=""/>
+
+           <form>
+               <input type="text" placeholder="Full Name (required if registering"/>
+               <input placeholder="profile picture URL (optional)" type="text"/>
+               <input placeholder="Email" type="email"/>
+               <input placeholder="Password" type="password"/>
+               <button>Sign In</button>
+           </form>
+           <p>Not a member?
+               <span className="login_register" onClick={register}>Register Now</span>
+           </p>
         </div>
     )
 }
