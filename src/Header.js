67ca91cae4f46987e66cac36621d224c6ecc7557
Rajeet <rajeetdps@gmail.com>
2021-07-17 20:39:38 +0530

    Styling the input buttons

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
                 alt="" />
                 <div className="header_search">
                     <SearchIcon/>
-                    <input type="text" />
+                    <input placeholder="Search" type="text" />
                 </div>
             </div>
             <div className="header_right">
--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { auth } from './firebase';
 import './Login.css';
 
 const Login = () => {
-    const register=()=>{
+    const [email, setEmail] = useState("");
+    const[password,setPassword]=useState("");
+    const[name,setName]=useState("");
+    const [profilePic, setProfilePic] = useState("");
 
+    const loginToApp=(e)=>{
+        e.preventDefault();
+        
+    };
+    const register=()=>{
+     
     };
     return (
         <div className="login">
-           <img src="https://www.amocrm.com/static/images/pages/integrations/logo/linkedin.png"  alt=""/>
+           <img src="https://www.amocrm.com/static/images/pages/integrations/logo/linkedin.png"  
+           alt=""/>
 
            <form>
-               <input type="text" placeholder="Full Name (required if registering"/>
-               <input placeholder="profile picture URL (optional)" type="text"/>
-               <input placeholder="Email" type="email"/>
-               <input placeholder="Password" type="password"/>
-               <button>Sign In</button>
+               <input 
+               type="text" 
+               placeholder="Full Name (required if registering"
+               name={name}
+               onChange={e=>setName(e.target.value)}
+               />
+               <input 
+               placeholder="profile picture URL (optional)" type="text"
+               value={profilePic}
+               onChange={e=>setProfilePic(e.target.value)}
+               />
+               <input 
+               placeholder="Email"
+                type="email" 
+                value={email}
+                onChange={e=>setEmail(e.target.value)}
+                />
+               <input
+                placeholder="Password"
+                 type="password"
+                 value={password}
+                 onChange={e=>setPassword(e.target.value)}
+                 />
+               <button type="submit" onClick={loginToApp}>Sign In</button>
            </form>
-           <p>Not a member?
+           <p>Not a member?{" "}
                <span className="login_register" onClick={register}>Register Now</span>
            </p>
         </div>
