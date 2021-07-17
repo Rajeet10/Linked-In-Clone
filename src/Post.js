3999e44668ef26f25c5bed8aed36304a81a1d99f
Rajeet <rajeetdps@gmail.com>
2021-07-17 14:57:25 +0530

    Added post buttons in feed section

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import InputOption from "./InputOption";
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+
 
 const Post = ({ name, description, message, photoUrl }) => {
     return (
@@ -8,12 +14,18 @@ const Post = ({ name, description, message, photoUrl }) => {
             <div className="post_header">
                 <Avatar />
                 <div className="post_info">
-                    <h2>Rajeet Chaudhray</h2>
-                    <p>Description</p>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
                 </div>
             </div>
             <div className="post_body">
-                <p>Message</p>
+                <p>{message}</p>
+            </div>
+            <div className="post_button">
+                <InputOption  Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+                <InputOption  Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <InputOption  Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <InputOption  Icon={SendOutlinedIcon} title="Send" color="gray" />
             </div>
         </div>
     );
