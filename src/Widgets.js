3b033a182c7ca6cf2e425ea77ea341562aba1952
Rajeet <rajeetdps@gmail.com>
2021-07-18 21:30:57 +0530

    addding widgets component

---
+++ src/Widgets.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Widgets.css';
+import InfoIcon from '@material-ui/icons/Info';
+const Widgets = () => {
+    return (
+        <div className="widgets">
+            <div className='widgets_header'>
+            <h2>Linked In News</h2>
+            <InfoIcon/>
+            </div>
+        </div>
+    )
+}
+
+export default Widgets
